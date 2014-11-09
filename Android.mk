cab5626f5373d8c624899e665436e8247764b1d5
Ethan Chen <intervigil@gmail.com>
2014-11-08 23:27:23 -0800

    display: Use project pathmap
    Change-Id: Iabcd263ecf9f4eb28f4cd53862ba663c81275c10

--- Android.mk
+++ Android.mk
@@ -1,10 +1,17 @@
+ifeq ($(call my-dir),$(call project-path-for,qcom-display))
+
 display-hals := libgralloc libgenlock libcopybit liblight
 display-hals += libhwcomposer liboverlay libqdutils libhdmi libqservice
 display-hals += libmemtrack
+
 ifeq ($(call is-vendor-board-platform,QCOM),true)
     include $(call all-named-subdir-makefiles,$(display-hals))
 else
 ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,$(display-hals))
 endif
+endif
+
+endif
+
 endif
